c2b6ed152bb586c93800873bc34ca9995210f65f
Edward <elok1@uci.edu>
2019-04-13 13:01:03 -0700

    Added Welcome Page - Testing Video Stream

---
+++ ATTHack4/Assets/Scripts/LoadInDisable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadInDisable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(waitDisable());
+    }
+
+    // Update is called once per frame
+    IEnumerator waitDisable()
+    {
+        
+        yield return new WaitForSeconds(3f);
+        this.gameObject.SetActive(false);
+    }
+}
