8caec52fb23fa943b08662c371758e572ec9ae8f
Edward <elok1@uci.edu>
2019-04-13 16:33:32 -0700

    Revert "Revert "Working Players""
    This reverts commit cb9a4b1794bd577d12aabf56519572ee1c5c6629.

---
+++ ATTHack4/Assets/Scripts/CharacterAttack1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPGCharacterAnims
+{
+    public class CharacterAttack1 : MonoBehaviour
+    {
+
+        RPGCharacterControllerFREE rpgCharacterController;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            InvokeRepeating("Attack", 1f, 2.5f);
+            rpgCharacterController = GetComponent<RPGCharacterControllerFREE>();
+        }
+
+        void Attack()
+        {
+            rpgCharacterController.AttackKick(1);
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ ATTHack4/Assets/Scripts/CharacterAttack2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPGCharacterAnims
+{
+    public class CharacterAttack2  : MonoBehaviour
+    {
+
+        RPGCharacterControllerFREE rpgCharacterController;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            InvokeRepeating("Attack", 1f, 1.5f);
+            rpgCharacterController = GetComponent<RPGCharacterControllerFREE>();
+        }
+
+        void Attack()
+        {
+            rpgCharacterController.AttackKick(1);    
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
